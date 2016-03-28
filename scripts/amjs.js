ce3392b6785b9506595fd48491413839ca131cb1
Vivek Singh <viveksingh1291@gmail.com>
2016-03-28 18:11:00 +0530

    modified

--- scripts/amjs.js
+++ scripts/amjs.js
@@ -1,33 +1,7 @@
 (function(){
     'use strict';
-
     var app = angular.module('MNEagle', ['ngMaterial'])
-    app.controller('AppCtrl', function($scope) {
-
-        $scope.data = [
-            {
-                who: 'Min Li Chan',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                who: 'Min Li Chan',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                who: 'Min Li Chan',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                who: 'Min Li Chan',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                who: 'Min Li Chan',
-                when: '3:08PM',
-                notes: " I'll be in your neighborhood doing errands"
-            }
-        ];
-    });
+    /*Color Theme for all pages*/
     app.config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('altTheme')
     });
@@ -65,4 +39,64 @@
                 'default': 'A200'
             })
     });
+
+  /*Data used for ng-repeat for several site*/
+    app.controller('AppCtrl', function($scope) {
+
+        $scope.data = [
+            {
+                who: 'Min Li Chan',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                who: 'Min Li Chan',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                who: 'Min Li Chan',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                who: 'Min Li Chan',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            }
+        ];
+    });
+
+    /*Bottom Sheet Script*/
+    app.controller('bottomSheetExample', function($scope) {
+
+    });
+
+    /*Date picker script    */
+    app.controller('datePic', function($scope) {
+       $scope.myDate= new Date();
+        $scope.minDate = new Date($scope.myDate.getFullYear(), $scope.myDate.getMonth() - 2, $scope.myDate.getDate());
+        $scope.maxDate = new Date($scope.myDate.getFullYear(), $scope.myDate.getMonth() + 2, $scope.myDate.getDate());
+        $scope.onlyWeekendsPredicate = function(date) {
+            var day = date.getDay()
+            return day===0 || day===6;
+        }
+    });
+    /*Checkbox script*/
+
+    app.controller('checkboxControl', function($scope) {
+        $scope.data = {};
+        $scope.data.cb1 = true;
+        $scope.data.cb2 = false;
+    });
+
+    app.controller('BasicDemoCtrl', DemoCtrl);
+
+    function DemoCtrl() {
+        var self = this;
+        self.readonly = false;
+        self.fruitNames = ['Apple', 'Banana', 'Orange'];
+        self.repeatFruitNames = angular.copy(self.fruitNames);
+    }
 })();
